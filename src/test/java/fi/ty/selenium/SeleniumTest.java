e5a277a164eeafa6b025068d5eed04d39ffedd82
Wiltzu <ville.ahti2@gmail.com>
2013-11-14 10:55:23 +0200

    changed test to TY and add some functionality

--- src/test/java/fi/ty/selenium/SeleniumTest.java
+++ src/test/java/fi/ty/selenium/SeleniumTest.java
@@ -1,12 +1,22 @@
 package fi.ty.selenium;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.google.common.base.Predicate;
 
 /**
  * Unit test for simple App.
@@ -14,18 +24,38 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class SeleniumTest {
 
 	private WebDriver driver;
+    private static final String baseUrl = "http://www.utu.fi/en/";
 	
 	@Before
 	public void setUp() {
 		System.out.println("creating driver...");
 		driver = new FirefoxDriver();
+        System.out.println("opening Turku Univesities page...");
+        driver.get(baseUrl);
 	}
 
     @Test
     public void testApp() {
-    	System.out.println("opening google...");
-    	driver.get("http://www.google.fi");
-        assertTrue( true );
+        List<WebElement> menuItems = driver.findElements(By.cssSelector(".menu-item-text"));
+        WebElement studyingMenuItem = null;
+        for(WebElement menuItem : menuItems) {
+            if(menuItem.getText().equals("Studying")) {
+                studyingMenuItem = menuItem;
+                break;
+            }
+        }
+        assertNotNull(studyingMenuItem);
+        
+        studyingMenuItem.click();
+        
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(new Predicate<WebDriver>() {
+			public boolean apply(WebDriver arg0) {
+				return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+			}
+        });
+        
+        assertEquals(baseUrl + "studying/Pages/home.aspx", driver.getCurrentUrl());    
     }
     
      @After
