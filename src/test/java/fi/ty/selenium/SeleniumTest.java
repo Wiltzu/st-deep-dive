abb3f2463fb0e1aa1b3d0de97fe182323a82a2a4
Wiltzu <ville.ahti2@gmail.com>
2013-11-14 13:33:16 +0200

    cool utu search bug test finished

--- src/test/java/fi/ty/selenium/SeleniumTest.java
+++ src/test/java/fi/ty/selenium/SeleniumTest.java
@@ -3,13 +3,17 @@ package fi.ty.selenium;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
 
+import org.apache.http.client.utils.URIBuilder;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -48,29 +52,52 @@ public class SeleniumTest {
 
         studyingMenuItem.click();
 
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        wait.until(new Predicate<WebDriver>() {
-            public boolean apply(WebDriver arg0) {
-                return ((JavascriptExecutor) driver).executeScript(
-                        "return document.readyState").equals("complete");
-            }
-        });
+        waitForPage();
 
 
         assertEquals(baseUrl + "studying/Pages/home.aspx",
                 driver.getCurrentUrl());
     }
 
+	private void waitForPage() {
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(new Predicate<WebDriver>() {
+            public boolean apply(WebDriver arg0) {
+                return ((JavascriptExecutor) driver).executeScript(
+                        "return document.readyState").equals("complete");
+            }
+        });
+	}
+
     @Test
     public void testSearch() {
+    	final String searchWord = "Studying";
         WebElement textField = driver.findElement(By.id("ctl00_PlaceHolderSearchArea_mossSearch_S2768ABB1_InputKeywords"));
         textField.click();
-        textField.sendKeys("Studying");
-        textField.submit();
+		textField.sendKeys(searchWord);
+        driver.findElement(By.id("ctl00_PlaceHolderSearchArea_mossSearch_S2768ABB1_go")).click();
+        sleep();
+        waitForPage();
+        try {
+			assertEquals(baseUrl + "search/Pages/results.aspx?k=" + URLEncoder.encode(searchWord, "UTF-8") , driver.getCurrentUrl());
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
     }
     
-    @After
+    private void sleep() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	@After
     public void tearDown() {
         System.out.println("Closing driver...");
         driver.quit();
