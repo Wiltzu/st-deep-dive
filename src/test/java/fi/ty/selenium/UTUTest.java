e1ced6ce76e4a223fb944ee374e2aedfc371739e
Wiltzu <ville.ahti2@gmail.com>
2013-11-21 11:45:01 +0200

    one more test

--- src/test/java/fi/ty/selenium/UTUTest.java
+++ src/test/java/fi/ty/selenium/UTUTest.java
@@ -7,13 +7,12 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.List;
 
-import org.apache.http.client.utils.URIBuilder;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -24,7 +23,7 @@ import com.google.common.base.Predicate;
 /**
  * Unit test for simple App.
  */
-public class SeleniumTest {
+public class UTUTest {
 
     private WebDriver driver;
     private static final String baseUrl = "http://www.utu.fi/en/";
@@ -33,27 +32,31 @@ public class SeleniumTest {
     public void setUp() {
         System.out.println("creating driver...");
         driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1024, 768));
         System.out.println("opening Turku Univesities page...");
         driver.get(baseUrl);
     }
 
     @Test
-    public void testApp() {
+    public void testStudyingTab() {
+    	//get all menu items with class
         List<WebElement> menuItems = driver.findElements(By
                 .cssSelector(".menu-item-text"));
+        
         WebElement studyingMenuItem = null;
         for (WebElement menuItem : menuItems) {
-            if (menuItem.getText().equals("Studying")) {
+            //find the element with text "Studying"
+        	if (menuItem.getText().equals("Studying")) {
                 studyingMenuItem = menuItem;
                 break;
             }
         }
+        
+        //assert element is not null and click it
         assertNotNull(studyingMenuItem);
-
         studyingMenuItem.click();
 
         waitForPage();
-
 
         assertEquals(baseUrl + "studying/Pages/home.aspx",
                 driver.getCurrentUrl());
@@ -72,30 +75,41 @@ public class SeleniumTest {
     @Test
     public void testSearch() {
     	final String searchWord = "Studying";
-        WebElement textField = driver.findElement(By.id("ctl00_PlaceHolderSearchArea_mossSearch_S2768ABB1_InputKeywords"));
-        textField.click();
-		textField.sendKeys(searchWord);
+        WebElement searchField = driver.findElement(By.id("ctl00_PlaceHolderSearchArea_mossSearch_S2768ABB1_InputKeywords"));
+        
+        searchField.click();
+		searchField.sendKeys(searchWord);
+		
+		//submit and wait
         driver.findElement(By.id("ctl00_PlaceHolderSearchArea_mossSearch_S2768ABB1_go")).click();
-        sleep();
         waitForPage();
+        
         try {
+        	//assert that url is the english URL
 			assertEquals(baseUrl + "search/Pages/results.aspx?k=" + URLEncoder.encode(searchWord, "UTF-8") , driver.getCurrentUrl());
+			//ignore the encoding exception
 		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
     }
     
-    private void sleep() {
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    @Test
+    public void testUTUShopLink() {
+    	final String utuShopURL = "https://utushop.utu.fi/";
+		WebElement utuShopLink = driver.findElement(By.cssSelector(String.format("a[href *= '%s']", utuShopURL)));
 		
-	}
+    	utuShopLink.click();
+    	
+    	waitForPage();
+    	
+    	//correct page is opened
+    	assertEquals(utuShopURL, driver.getCurrentUrl());
+    		
+    	WebElement productsElement = driver.findElement(By.className("section-header"));
+    	//Page is in english
+    	assertEquals("PRODUCTS", productsElement.getText());
+    }
 
 	@After
     public void tearDown() {
